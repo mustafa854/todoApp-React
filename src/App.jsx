17c79ee318422e3e0fab879e4b988bf221277c72
Mustafa <mustafabidiwala84@gmail.com>
2023-12-09 22:38:18 +0530

    removed extra files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { TodoContext, TodoProvider } from "./contexts/todoContext";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
